d390097b716ba61c444d1f8c7b0f4287369d24cf
Tobias Andrade <tobias.decamposandrade@gmail.com>
2020-09-23 17:01:13 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/tobias/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.tobias.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tobias.cursomc.domain.Cliente;
+import com.tobias.cursomc.repositories.ClienteRepository;
+import com.tobias.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
