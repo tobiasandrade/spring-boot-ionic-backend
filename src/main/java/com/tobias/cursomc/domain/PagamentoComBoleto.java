be1f27602703c91126c029ef92dfd26678d1c9fb
Tobias Andrade <tobias.decamposandrade@gmail.com>
2020-09-25 11:26:16 -0300

    Endpoint /pedidos/{id} diponibilizado

--- src/main/java/com/tobias/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/tobias/cursomc/domain/PagamentoComBoleto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tobias.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -11,7 +12,9 @@ public class PagamentoComBoleto extends Pagamento{
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVenciemento;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
---
+++ src/main/java/com/tobias/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.tobias.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tobias.cursomc.domain.Pedido;
+import com.tobias.cursomc.repositories.PedidoRepository;
+import com.tobias.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
